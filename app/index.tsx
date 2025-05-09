7c3eac7506b77d2875622829693177d9a5a5e204
Kyrylo <dexter37814@gmail.com>
2025-05-09 20:26:27 +0300

    styles: set tailwind palette

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from 'react-native'
 
 export default function Index() {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-        >
-            <Text className='text-3xl'>Welcome!</Text>
+        <View className='flex-1 justify-center items-center'>
+            <Text className='text-3xl text-accent'>Welcome!</Text>
         </View>
     )
 }
