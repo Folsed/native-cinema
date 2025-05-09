9d295697fccd92c97ae0bb74baeea5169f0ea0a5
Kyrylo <dexter37814@gmail.com>
2025-05-09 21:11:19 +0300

    feat: expo router

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,12 @@
 import { Text, View } from 'react-native'
+import { Link } from 'expo-router'
 
 export default function Index() {
     return (
         <View className='flex-1 justify-center items-center'>
             <Text className='text-3xl text-accent'>Welcome!</Text>
+            <Link href='/onboarding'>Go to Onboarding</Link>
+            <Link href='/movie/expedition-33'>Go to Movie</Link>
         </View>
     )
 }
